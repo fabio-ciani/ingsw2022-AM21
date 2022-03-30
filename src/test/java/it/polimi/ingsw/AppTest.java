edf5f93b415848e80cad3345916fc567e7e0d663
Davide Mozzi <davide.mozzi@mail.polimi.it>
2022-03-30 23:38:06 +0200

    Edit AppTest example to work with JUnit 5

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,14 +1,13 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */

8dc78b0ff23f971ab6864bec27c1c9e2a5dc67b6
Fabio Ciani <fabio1.ciani@mail.polimi.it>
2022-04-07 11:51:41 +0200

    Update influence package tests

--- src/test/java/it/polimi/ingsw/eriantys/model/influence/BonusInfluenceTest.java
+++ src/test/java/it/polimi/ingsw/eriantys/model/influence/BonusInfluenceTest.java
@@ -1,20 +1,42 @@
 package it.polimi.ingsw.eriantys.model.influence;
 
+import it.polimi.ingsw.eriantys.model.Bag;
+import it.polimi.ingsw.eriantys.model.Color;
+import it.polimi.ingsw.eriantys.model.IslandGroup;
 import it.polimi.ingsw.eriantys.model.Player;
 import it.polimi.ingsw.eriantys.model.exceptions.IllegalInfluenceStateException;
+import it.polimi.ingsw.eriantys.model.exceptions.NoMovementException;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusInfluenceTest {
+    InfluenceCalculator calc = new BonusInfluence(new Player("admin"));
+
     @Test
     void BonusInfluence_PassNull_ThrowException() {
-        InfluenceCalculator calc = new BonusInfluence(new Player("admin"));
-
         assertThrowsExactly(IllegalInfluenceStateException.class, () -> calc.calculate(new Player("Eve"), null, null));
     }
 
     @Test
-    void BonusInfluence_ValidParameters_NormalPostConditions() {
+    void BonusInfluence_ValidParameters_NormalPostConditions() throws IllegalInfluenceStateException, NoMovementException {
+        Player p = new Player("admin");
+        IslandGroup island = new IslandGroup("03");
+        Set<Color> professors = new HashSet<>();
+
+        island.setController(p);
+        professors.add(Color.PINK);
+        professors.add(Color.BLUE);
+
+        Bag b = new Bag();
+
+        b.moveTo(island, Color.PINK);
+        b.moveTo(island, Color.PINK);
+        b.moveTo(island, Color.RED);
+
+        assertEquals(3 + 2, calc.calculate(p, island, professors));
     }
 }

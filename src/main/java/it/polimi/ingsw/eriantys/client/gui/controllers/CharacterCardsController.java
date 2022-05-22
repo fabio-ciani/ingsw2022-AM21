1e9b6160e266b205a1439ed642bf07ecb548673b
Fabio Ciani <fabio1.ciani@mail.polimi.it>
2022-05-22 19:48:28 +0200

    Add grayscale effect example to JavaFX sources

--- src/main/java/it/polimi/ingsw/eriantys/client/gui/controllers/CharacterCardsController.java
+++ src/main/java/it/polimi/ingsw/eriantys/client/gui/controllers/CharacterCardsController.java
@@ -3,9 +3,11 @@ package it.polimi.ingsw.eriantys.client.gui.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tooltip;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -17,9 +19,9 @@ public class CharacterCardsController extends Controller implements Initializabl
 
 	public CharacterCardsController() {
 		characters = new String[3];
-		characters[0] = "HerbGranny";
-		characters[1] = "MushroomGuy";
-		characters[2] = "Jester";
+		characters[0] = "Knight";
+		characters[1] = "Minstrel";
+		characters[2] = "Thief";
 	}
 
 	@Override
@@ -36,7 +38,12 @@ public class CharacterCardsController extends Controller implements Initializabl
 
 					img.setImage(new Image(getClass().getResource("/graphics/CharacterCards/" + characters[Character.getNumericValue(x.getId().charAt(1))] + ".jpg").toExternalForm()));
 
-					Tooltip desc = new Tooltip("You are hovering on the " + characters[Character.getNumericValue(x.getId().charAt(1))] + " card.");
+					Rectangle clip = new Rectangle(img.getFitWidth(), img.getFitHeight());
+					clip.setArcWidth(30);
+					clip.setArcHeight(30);
+					img.setClip(clip);
+
+					Tooltip desc = new Tooltip("You are hovering on the " + split(characters[Character.getNumericValue(x.getId().charAt(1))]) + " card.");
 					// desc.setShowDelay(Duration.ZERO);
 					Tooltip.install(img, desc);
 				});
@@ -45,6 +52,26 @@ public class CharacterCardsController extends Controller implements Initializabl
 	private void drawLabels() {
 		cards.getChildren().stream()
 				.filter(x -> x instanceof Text && x.getId().matches("^\\w+text\\z"))
-				.forEach(x -> ((Text) x).setText(characters[Character.getNumericValue(x.getId().charAt(1))]));
+				.forEach(x -> ((Text) x).setText(split(characters[Character.getNumericValue(x.getId().charAt(1))])));
+	}
+
+	private String split(String in) {
+		StringBuilder out = new StringBuilder();
+		char c;
+
+		for (int i = 0; i < in.length(); i++) {
+			c = in.charAt(i);
+			if (i != in.length() - 1 && Character.isLowerCase(c) && Character.isUpperCase(in.charAt(i + 1)))
+				out.append(c).append(' ');
+			else out.append(c);
+		}
+
+		return out.toString();
+	}
+
+	private void applyGrayscale(ImageView img) {
+		ColorAdjust grayscale = new ColorAdjust();
+		grayscale.setSaturation(-1);
+		img.setEffect(grayscale);
 	}
 }

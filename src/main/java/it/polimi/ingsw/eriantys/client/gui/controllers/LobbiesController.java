741dcdd96900f489efe35b57ec813a79ce0411bc
Tommaso Bonetti <tommaso2.bonetti@mail.polimi.it>
2022-05-23 22:07:32 +0200

    Add partial implementation of LobbiesController

--- src/main/java/it/polimi/ingsw/eriantys/client/gui/controllers/LobbiesController.java
+++ src/main/java/it/polimi/ingsw/eriantys/client/gui/controllers/LobbiesController.java
@@ -1,68 +1,79 @@
 package it.polimi.ingsw.eriantys.client.gui.controllers;
 
+import it.polimi.ingsw.eriantys.controller.GameInfo;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class LobbiesController extends Controller {
-	@FXML private TableView lobbies;
-	@FXML private TableColumn identificators, creators, players, modes;
+public class LobbiesController extends Controller implements Initializable {
+	@FXML private TableView<Lobby> lobbies;
+	@FXML private TableColumn<Lobby, Integer> identifiers;
+	@FXML private TableColumn<Lobby, String> creators;
+	@FXML private TableColumn<Lobby, String> players;
+	@FXML private TableColumn<Lobby, String> modes;
+	@FXML private Button confirm;
+	@FXML private Button refresh;
 
 	public LobbiesController() {
-		lobbies = new TableView<Lobby>();
-		identificators = new TableColumn<Lobby, Integer>();
-		creators = new TableColumn<Lobby, String>();
-		players = new TableColumn<Lobby, String>();
-		modes = new TableColumn<Lobby, String>();
+		lobbies = new TableView<>();
+		identifiers = new TableColumn<>();
+		creators = new TableColumn<>();
+		players = new TableColumn<>();
+		modes = new TableColumn<>();
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
-		/*
-		lobbies.getColumns().stream()
-				.forEach(x -> System.out.println(((TableColumn) x).getCellValueFactory()));
-		*/
-
-		identificators.setCellValueFactory(new PropertyValueFactory<>("id"));
+		identifiers.setCellValueFactory(new PropertyValueFactory<>("id"));
 		creators.setCellValueFactory(new PropertyValueFactory<>("creator"));
 		players.setCellValueFactory(new PropertyValueFactory<>("counter"));
 		modes.setCellValueFactory(new PropertyValueFactory<>("expert"));
 
+		confirm.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			System.out.println("confirm - mouse clicked");
+			Lobby selectedLobby = lobbies.getSelectionModel().getSelectedItem();
+			/*
+			if (selectedLobby == null)
+				ui.showError("Please select a lobby!");
+			else
+				client.joinLobby(selectedLobby.getId());
+			 */
+		});
+
+		refresh.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+			System.out.println("refresh - mouse clicked");
+			// client.askLobbies();
+		});
+
+		// TODO client.askLobbies();
+		// TODO upon receiving AvailableLobbies, check if this scene is active and populate the table
+		/*
 		lobbies.getItems().add(new Lobby(1, "JohnDoe", "1/3", true));
 		lobbies.getItems().add(new Lobby(2, "MarioRossi", "1/2", false));
+	 */
 	}
 
-	private static class Lobby {
-		private Integer id;
-		private String creator;
-		private String counter;
-		private Boolean expert;
-
-		private Lobby(Integer id, String creator, String counter, Boolean expert) {
-			this.id = id;
-			this.creator = creator;
-			this.counter = counter;
-			this.expert = expert;
+	public void updateLobbies(List<GameInfo> availableLobbies) {
+		ObservableList<Lobby> content = lobbies.getItems();
+		content.clear();
+		for (GameInfo l : availableLobbies) {
+			String counter = l.getCurrentPlayers() + "/" + l.getLobbySize();
+			content.add(new Lobby(l.getGameId(), l.getCreator(), counter, l.isExpertMode()));
 		}
+	}
 
-		public Integer getId() {
+	private record Lobby(Integer id, String creator, String counter, Boolean expert) {
+		public int getId() {
 			return id;
-		}
-
-		public String getCreator() {
-			return creator;
-		}
-
-		public String getCounter() {
-			return counter;
-		}
-
-		public Boolean getExpert() {
-			return expert;
 		}
 	}
 }
